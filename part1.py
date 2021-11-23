3ea6c6f124c9588c0eedd78c51c6e892163e2e95
Matthew Fields <93930212+HelloWorld314159@users.noreply.github.com>
2021-11-23 21:06:01 +0000

    Modified code to compare three numbers

--- part1.py
+++ part1.py
@@ -15,13 +15,19 @@ Enter another number: 5
 The smallest number is 2
 '''
 
-number = int(input("Enter a number: "))
+number1 = int(input("Enter a number: "))
 
-smallest = number
+number2 = int(input("Enter another number: "))
 
-number = int(input("Enter another number: "))
+number3 = int(input("Enter a third number: "))
 
-if number < smallest:
-  smallest = number
-  
-print("The smallest number is ", smallest)
+if number1 < number2:
+  if number1 < number3:
+    print("The smallest number is ", number1)
+if number2 < number3:
+  if number2 < number1:
+    print("The smallest number is", number2)
+if number3 < number2:
+  if number3 < number1:
+    print("The smallest number is", number3)
+
